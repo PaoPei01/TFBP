f624cb8cebd4ac4acfb4bb3dccccfd226694fabc
PaoPei01 <paopei2706@gmail.com>
2026-05-19 21:55:01 +0700

    Use hash router for GitHub Pages

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import { App } from './App';
 import { LanguageProvider } from './context/LanguageContext';
 import './styles.css';
@@ -8,9 +8,9 @@ import './styles.css';
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <LanguageProvider>
-      <BrowserRouter basename={import.meta.env.BASE_URL}>
+      <HashRouter>
         <App />
-      </BrowserRouter>
+      </HashRouter>
     </LanguageProvider>
   </StrictMode>,
 );
